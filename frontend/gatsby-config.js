f90fb0dfdd460ac911f4b70c7724c5f01a9f9f84
Ainars <ainars.c@gmail.com>
2021-08-07 10:47:56 -0600

    Add strapi to config

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -45,5 +45,14 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Defaults to 100
+        collectionTypes: [`photo`, `gallery`],
+        singleTypes: [`home-page`, `contact`],
+      },
+    },
   ],
 };
