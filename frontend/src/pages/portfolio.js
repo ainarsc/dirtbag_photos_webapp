da90f003d97538be05492f87b0680ef71ebfeedf
Ainars <ainars.c@gmail.com>
2021-08-29 11:04:47 -0600

    Refactoring

--- frontend/src/pages/portfolio.js
+++ frontend/src/pages/portfolio.js
@@ -1,28 +1,10 @@
-import * as React from "react"
-import Layout from '../components/layout'
-import styled from 'styled-components'
-import GalleryListing from '../components/portfolio/galleryListing'
-import {graphql} from 'gatsby'
+import * as React from "react";
+import Layout from "../components/layout";
+import styled from "styled-components";
+import { graphql, link } from "gatsby";
+import Image from "../components/portfolio/Image";
 
-
-
-// markup
-const Portfolio = ({data}) => {
-  const {allStrapiGallery} = data
-
-  return (
-      <Layout>
-        <Wrapper>
-          <GalleryListing edges={allStrapiGallery.edges} />
-          <GalleryListing edges={allStrapiGallery.edges} />
-          <GalleryListing edges={allStrapiGallery.edges} />
-          <GalleryListing edges={allStrapiGallery.edges} />
-          <GalleryListing edges={allStrapiGallery.edges} />
-        </Wrapper>
-      </Layout>
-  )
-}
-
+//GRAPHQL QUERY
 export const pageQuery = graphql`
   query {
     allStrapiGallery {
@@ -43,7 +25,32 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
+
+//MARKUP
+const Portfolio = ({ data }) => {
+  const { allStrapiGallery } = data;
+
+  return (
+    <Layout>
+      <Wrapper>
+        {allStrapiGallery.edges.map(({ node }) => (
+          <GalleryCard>
+            <Link to="/">
+              <ImgBox>
+                <Image image={node.thumbnail.localFile} />
+              </ImgBox>
+
+              <GalleryTitleBox>
+                <Title>Placeholder</Title>
+              </GalleryTitleBox>
+            </Link>
+          </GalleryCard>
+        ))}
+      </Wrapper>
+    </Layout>
+  );
+};
 
 const Wrapper = styled.div`
   width: 100%;
@@ -53,7 +60,32 @@ const Wrapper = styled.div`
   align-content: flex-start;
   justify-content: space-between;
   padding: 4rem 2rem 2rem 2rem;
+`;
 
-`
+const GalleryCard = styled.div`
+  min-width: 50%;
+  padding: 2px;
+  border-radius: 10px;
+  &:hover {
+    div${ImgBox} {
+      opacity: 1;
+    }
+  }
+`;
+const GalleryTitleBox = styled.div`
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(25, 25, 25);
+`;
+const Title = styled.span`
+  font-size: 1em;
+  letter-spacing: 8px;
+`;
+const ImgBox = styled.div`
+  opacity: 0.8;
+`;
 
-export default Portfolio
+export default Portfolio;
