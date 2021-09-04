069018981f6098d19694257dbef1844e18f90fa4
Ainars <ainars.c@gmail.com>
2021-09-04 11:58:16 -0600

    Markup and dummy text

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,36 +1,50 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { graphql } from "gatsby";
+import styled from "styled-components";
 
-// export const pageQuery = graphql`
-//   query {
-//     allStrapiGallery {
-//       edges {
-//         node {
-//           link
-//           thumbnail {
-//             localFile {
-//               childImageSharp {
-//                 gatsbyImageData(
-//                   aspectRatio: 1.7
-//                   layout: FULL_WIDTH
-//                   placeholder: DOMINANT_COLOR
-//                 )
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
+export const pageQuery = graphql`
+  query {
+    strapiAbout {
+      Content
+      Title
+    }
+  }
+`;
 
 // markup
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const { Content, Title } = data.strapiAbout;
+
   return (
     <Layout>
-      <div></div>
+      <Wrapper>
+        <Headline>{Title}</Headline>
+        <Text>{Content}</Text>
+      </Wrapper>
     </Layout>
   );
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: flex-start;
+
+  padding: 4rem 2rem 2rem 2rem;
+`;
+
+const Headline = styled.h1`
+  font-size: 25px;
+`;
+
+const Text = styled.p`
+  font-size: 20px;
+  letter-spacing: 2px;
+  line-height: 30px;
+  padding: 2rem 5rem;
+`;
 
 export default IndexPage;
