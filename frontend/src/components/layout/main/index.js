2ca0a8dba8f56070fa12f3ea99f7beeb4f57260e
Ainars <ainars.c@gmail.com>
2022-02-23 21:11:13 -0700

    Remove overflow, DS_Store

--- frontend/src/components/layout/main/index.js
+++ frontend/src/components/layout/main/index.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import styled from 'styled-components';
-
+import React from "react";
+import styled from "styled-components";
 
 export const Main = ({ children }) => {
-
-    return (
-        <Wrapper>
-            {children}
-        </Wrapper>
-    )
-}
+  return <Wrapper>{children}</Wrapper>;
+};
 
 const Wrapper = styled.div`
-    /* background-color: ${({theme}) => theme.primaryDark}; */
-    height: 97vh;
-    /* position: relative; */
-    width: 100%;
-    overflow: scroll;
-    /* flex-direction: column;
+  /* background-color: ${({ theme }) => theme.primaryDark}; */
+  height: 97vh;
+  padding: 0;
+  /* position: relative; */
+  width: 100%;
+  /* flex-direction: column;
     align-items: center;
     justify-content: space-evenly; */
-`
+`;
